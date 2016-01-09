df1e26f9c2987efbb140b9334c3be8ae1ad050bc
sx4452 <zb.benzhang@gmail.com>
2016-01-09 17:04:06 +0800

    201601091704

---
+++ TranslationRulesExtraction.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+__auther__ = 'ben'
+
+import time
+import codecs
+import string
+
+def TranslationRuleExtr(testch, testen, testalign):
+    lengthCH = len(testalign)
+    Trans = {}
+    for i in xrange(1, lengthCH):
+        for j in xrange(1, len(testalign[i])):
+            index = testalign[i][j].split('-')
+            index1 = int(index[0])
+            index2 = int(index[1])
+            strCH = testch[i][index1]
+            strEN = testen[i][index2]
+            if not Trans.has_key(strCH):
+                Trans[strCH] = {}
+            if Trans[strCH].has_key(strEN):
+                Trans[strCH][strEN] += 1
+            else:
+                Trans[strCH][strEN] = 1
+    Freq = Trans
+    for strCH in Trans:
+        allcnt = 0
+        for strEN in Trans[strCH]:
+            allcnt = allcnt + Trans[strCH][strEN]
+        for strEN in Freq[strCH]:
+            Freq[strCH][strEN] = float(Trans[strCH][strEN])/float(allcnt)
+    OutputStr = ''
+    for strCH in Freq:
+        for strEN in Freq[strCH]:
+            curStr = strCH + ' ||| '+ strEN + ' ||| ' + str(Freq[strCH][strEN])
+            OutputStr = OutputStr + curStr + '\r\n'
+    return OutputStr
+
+def InputData(fileName):
+    Data = {}
+    dataCnt = 0
+    fileData = codecs.open(fileName, 'r', 'utf-8')
+    isDone = 0
+    while not isDone:
+        line = fileData.readline()
+        if not line:
+            isDone = 1
+        else:
+            lineSplit = line.split(' ')
+            lineSplit[-1] = lineSplit[-1][:-1]
+            Data[dataCnt] = lineSplit
+            dataCnt += 1
+    fileData.close()
+    return Data
+
+def main():
+    print 'do main'
+    testch = InputData('test.ch.txt')
+    testen = InputData('test.en.txt')
+    testalign = InputData('test.align.txt')
+    lengthCH = len(testch)
+    lengthEN = len(testen)
+    lengthALIGN = len(testalign)
+    if lengthCH != lengthEN or lengthCH != lengthALIGN or lengthEN != lengthALIGN:
+        print 'data size error!'
+    output = TranslationRuleExtr(testch, testen, testalign)
+    outputFile = open('output.txt', 'w')
+    outputFile.write(output)
+    outputFile.close()
+
+if __name__ == '__main__':
+    start = time.clock()
+    main()
+    end = time.clock()
+    print 'elapsed time is'
+    print end - start
